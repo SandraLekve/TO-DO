3e41eefe296a9081e4585e99ac0a8b8985d68cf1
sandra <sandra.lekve@icloud.com>
2022-07-04 20:51:02 +0200

    install react-router-dom, adding Routing

---
+++ src/Components/Focus.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Focus = () => {
+    return(
+    <h1>Hello Focus</h1>
+    );
+}
+
+export default Focus;
---
+++ src/Components/Liste.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Liste: React.FC<Props> = () => {
+  return <h1>Hello, List </h1>;
+};
+
+export default Liste;
